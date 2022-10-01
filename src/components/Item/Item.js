09ae913be120884b8819ed2b0f8370c2351fcef1
Mariano Cuartero <cuarteroma@gmail.com>
2022-10-01 10:18:51 -0300

    Agregado funcionalidades varias

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -4,6 +4,8 @@ const Item = ({title, price, image}) => {
   return (
     <div className="item-listed">
     <img src={image} alt={title} />
+    <br></br>
+    <br></br>
     <h2>{title}</h2>
     <p>${price}.-</p>
     </div>
